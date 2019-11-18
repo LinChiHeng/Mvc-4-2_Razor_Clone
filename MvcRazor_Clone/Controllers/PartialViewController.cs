d235d0ef10e6268984d35a0d0d8f2c4dd4a95b01
Henry Lin <chlin97701021@hotmail.com>
2019-11-18 18:27:58 +0800

    完成範例4-6

---
+++ MvcRazor_Clone/Controllers/PartialViewController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcRazor_Clone.Controllers
+{
+    public class PartialViewController : Controller
+    {
+        // GET: PartialView
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult SimpleCard()
+        {
+            return View();
+        }
+    }
+}
